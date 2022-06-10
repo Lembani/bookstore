acc1fde2632424079eef2f4ee228df42c07a928c
Lembani Sakala <lembanisakala@gmail.com>
2022-06-10 07:07:09 +0200

    Update AddBook with state and event handler

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const AddBook = () => (
-  <>
-    <div className="add-book-container">
-      <h2>Add Book</h2>
-      <form action="#" method="post">
-        <input type="text" placeholder="Book title" />
-        <input type="text" placeholder="Author" />
-        <button type="button">Add Book</button>
-      </form>
-    </div>
-  </>
-);
+const AddBook = () => {
+  const [data, setData] = useState({ book: '', author: '' });
+
+  const inputData = (e) => {
+    const inputField = e.target.name;
+    switch (inputField) {
+      case 'title':
+        setData({ book: e.target.value, author: data.author });
+        break;
+      case 'author':
+        setData({ author: e.target.value, book: data.book });
+        break;
+      default:
+        setData({ book: data.book, author: data.author });
+    }
+  };
+
+  const dataStore = () => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <div className="add-book-container">
+        <h2>Add Book</h2>
+        <form action="#" method="post">
+          <input type="text" name="title" onChange={inputData} placeholder="Book title" />
+          <input type="text" name="author" onChange={inputData} placeholder="Author" />
+          <button type="button" onClick={dataStore}>Add Book</button>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default AddBook;
