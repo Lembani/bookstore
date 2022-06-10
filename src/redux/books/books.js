89b110c0ccf5d46a6d18be949fd2bfc4af41255f
Lembani Sakala <lembanisakala@gmail.com>
2022-06-10 20:43:29 +0200

    Update books reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,15 +1,31 @@
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
-const books = [];
+const books = [
+  {
+    id: 1,
+    title: 'The Richest Man In Babylon',
+    author: 'George Samuel Clason',
+  },
+  {
+    id: 2,
+    title: 'The Lean Startup',
+    author: 'Eric Ries',
+  },
+  {
+    id: 3,
+    title: 'This Is Marketing',
+    author: 'Seth Godin',
+  },
+];
 
 const booksReducer = (state = books, action) => {
   switch (action.type) {
     case ADD_BOOK: {
       const bookObj = {
         id: state.length + 1,
+        title: action.book.book,
         author: action.book.author,
-        book: action.book.book,
       };
       return [...state, bookObj];
     }
