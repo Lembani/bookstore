b78689d11e2a9ff46fba296e17307c4564f7fd51
Lembani Sakala <lembanisakala@gmail.com>
2022-06-11 16:00:32 +0200

    Update `configureStore` with applyMiddleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,14 @@
-import { createStore, combineReducers } from 'redux';
-import booksReducer from './books/books';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { booksReducer } from './books/books';
 import categories from './categories/categories';
+import logger from './logger';
 
 const reducers = combineReducers({
   booksReducer,
   categories,
 });
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(logger, thunk));
 
 export default store;
