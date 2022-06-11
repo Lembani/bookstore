4278685e0d36456d14318cbc0cd0cc798f1158a2
Lembani Sakala <lembanisakala@gmail.com>
2022-06-11 16:17:08 +0200

    Update `booksReducer`

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,38 +1,41 @@
+import axios from 'axios';
+
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const FETCH_BOOK = 'bookstore/books/FETCH_BOOK';
 
-const books = [
-  {
-    id: 1,
-    title: 'The Richest Man In Babylon',
-    author: 'George Samuel Clason',
-  },
-  {
-    id: 2,
-    title: 'The Lean Startup',
-    author: 'Eric Ries',
-  },
-  {
-    id: 3,
-    title: 'This Is Marketing',
-    author: 'Seth Godin',
-  },
-];
+const baseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/ysdubPf88eKEHHCzHP2V/books';
 
-const booksReducer = (state = books, action) => {
+const books = [];
+
+export const booksReducer = (state = books, action) => {
   switch (action.type) {
     case ADD_BOOK: {
       const bookObj = {
-        id: state.length + 1,
+        item_id: `book${Date.now()}`,
         title: action.book.book,
         author: action.book.author,
+        category: 'business',
       };
-      return [...state, bookObj];
+
+      const newStateData = [`book${Date.now()}`, [{
+        title: action.book.book,
+        author: action.book.author,
+        category: 'business',
+      }]];
+
+      axios.post(baseUrl, bookObj);
+      return [...state, newStateData];
     }
+
     case REMOVE_BOOK: {
-      const remove = state.filter((book) => book.id !== action.id);
-      return remove;
+      axios.delete(`${baseUrl}/${action.id}`);
+      const newData = state.filter((book) => book[0] !== action.id);
+      return newData;
     }
+
+    case FETCH_BOOK:
+      return action.newBook;
     default:
       return state;
   }
@@ -42,4 +45,11 @@ export const addBook = (book) => ({ type: ADD_BOOK, book });
 
 export const removeBook = (id) => ({ type: REMOVE_BOOK, id });
 
-export default booksReducer;
+export const getBooks = () => (dispatch) => {
+  axios.get(baseUrl).then(
+    (response) => {
+      const data = Object.entries(response.data);
+      dispatch({ type: FETCH_BOOK, newBook: data });
+    },
+  );
+};
